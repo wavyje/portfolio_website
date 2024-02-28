a299057a4bc622ef2cb55877d0531aa30e01f569
wavyje <jendrik.mann@uni-oldenburg.de>
2024-02-28 12:22:29 +0100

    changed flex for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <main className={"flex min-h-screen flex-col items-center justify-center"}>
       <div className="flex min-h-screen items-center justify-center flex-col space-y-52">
-        <div className={"flex"}>
+        <div className={"flex flex-col sm:flex-row"}>
           <h1 className={"text-3xl font-bold text-white"}>Jendrik Mann</h1>
-          <div className={"mt-14 ml-5 hover-opacity:100 duration-300"}>
+          <div className={"mt-14 ml-5"}>
             <h1 className={"text-2xl font-semibold text-white"}>
               full-stack developer
             </h1>
